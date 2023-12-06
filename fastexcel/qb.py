e03e8d3ba127d385c5a04397de3843fc7ce40420
Charleenlozi <clozymwangs@gmail.com>
2023-12-06 22:11:37 +0300

    fix: getting auth url function redirect breaking

--- fastexcel/qb.py
+++ fastexcel/qb.py
@@ -9,7 +9,7 @@ load_dotenv()
 client_id = os.getenv('CLIENTID')
 client_secret = os.getenv('CLIENTSECRET')
 
-
+#function to get the auth_url for the user to authenticate the app and return auth_code
 def get_auth_url():
     auth_client = AuthClient(client_id=client_id, client_secret=client_secret, environment='sandbox',
                             redirect_uri='http://localhost:8000/upload')
