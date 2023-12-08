e0565ac70d937ed9db28c1b8f6e4bad81711a3e9
Charleenlozi <clozymwangs@gmail.com>
2023-12-08 18:58:31 +0300

    feat: getting access_token

--- fastexcel/pay.py
+++ fastexcel/pay.py
@@ -3,13 +3,18 @@
 import requests
 
 
-class MpesaPay():
-    def __init__(self, phone, amount, description):
+class MpesaPay:
+    def __init__(self, phone=None, amount=None):
         self.phone = phone
         self.amount = amount
 
     def authorization(self):
-        response = requests.request("GET", 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials', headers = { 'Authorization': 'Bearer cFJZcjZ6anEwaThMMXp6d1FETUxwWkIzeVBDa2hNc2M6UmYyMkJmWm9nMHFRR2xWOQ==' })
-        return response.text.encode('utf8')
+        response = requests.request("GET", 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials', headers = { 'Authorization': 'Basic cFJZcjZ6anEwaThMMXp6d1FETUxwWkIzeVBDa2hNc2M6UmYyMkJmWm9nMHFRR2xWOQ==' })
+        access_token = response.text.encode('utf8')
+        return access_token
 
 
+
+access_token = MpesaPay().authorization() 
+
+print(access_token)
