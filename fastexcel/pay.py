2d6335b55d0b16d54c0b7793f6a7da2afac81d24
Charleenlozi <clozymwangs@gmail.com>
2023-12-08 11:13:56 +0300

    feat: Mpesa class  functions API

--- fastexcel/pay.py
+++ fastexcel/pay.py
@@ -1,6 +1,15 @@
-#creating payment functions to intergrate with Daraja API 
+#creating payment class functions to intergrate with Daraja API 
 #using mpessa express API
+import requests
 
 
-def authorization():
-    pass
+class MpesaPay():
+    def __init__(self, phone, amount, description):
+        self.phone = phone
+        self.amount = amount
+
+    def authorization(self):
+        response = requests.request("GET", 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials', headers = { 'Authorization': 'Bearer cFJZcjZ6anEwaThMMXp6d1FETUxwWkIzeVBDa2hNc2M6UmYyMkJmWm9nMHFRR2xWOQ==' })
+        return response.text.encode('utf8')
+
+
